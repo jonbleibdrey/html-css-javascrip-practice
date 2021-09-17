670f1456d3b1fc266add2f2c7d97c3adfa3be1d0
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-09-17 13:21:34 -0400

    added option bar

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Body = () => {
+  const [toggle, setToggle] = useState(false)
+
+  
+
     return (
         <div>
-            hello we in teh body now
+            <button onClick={()=> setToggle(!toggle)}>Options 
+            {toggle ? (
+            <div>
+            <select name="" id="">
+                <option value="">about</option>
+                <option value="">create</option>
+                <option value="">read</option>
+            </select>
+            </div>
+             ) : (
+            null
+            )}
+            </button>
+           
         </div>
     )
 }
